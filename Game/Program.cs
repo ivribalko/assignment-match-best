4f749a34079ca829d3f721c62eaa1d353021b02f
Ivan Rybalko <ivribalko@gmail.com>
2020-05-12 17:19:12 -0400

    original class

--- Game/Program.cs
+++ Game/Program.cs
@@ -9,4 +9,43 @@ namespace Game
             Console.WriteLine("Hello World!");
         }
     }
+
+    Public class Board
+    {
+    enum JewelKind
+    {
+           Empty,
+           Red,
+           Orange,
+           Yellow
+           Green,
+           Blue,
+           Indigo,
+           Violet
+    };
+
+    enum MoveDirection
+    {
+           Up,
+           Down,
+           Left,
+           Right
+    };
+
+    struct Move
+    {
+    int x;
+    int y;
+    MoveDirection direction;
+    };
+
+           int GetWidth();
+           int GetHeight();
+
+           JewelKind GetJewel(int x, int y);
+           void SetJewel(int x, int y, JewelKind kind);
+
+    //Implement this function
+    Public Move CalculateBestMoveForBoard();
+    };
 }
