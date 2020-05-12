74fd61e95415956583ec3cb9280af12f23631cbf
Ivan Rybalko <ivribalko@gmail.com>
2020-05-12 19:35:30 -0400

    iterate over all

--- Game/Predict.cs
+++ Game/Predict.cs
@@ -1,18 +1,53 @@
+using System;
+using System.Linq;
+
 namespace Game
 {
     internal static class Predict
     {
+        private static readonly MoveDirection[] Directions = Enum
+            .GetValues(typeof(MoveDirection))
+            .Cast<MoveDirection>()
+            .ToArray();
+
         internal static Move? BestMove(JewelKind[,] jewels)
         {
-            for (int x = 0; x < jewels.GetLength(0); x++)
+            var maxx = jewels.GetLength(0);
+            var maxy = jewels.GetLength(1);
+
+            for (int x = 0; x < maxx; x++)
             {
-                for (int y = 0; y < jewels.GetLength(1); y++)
+                for (int y = 0; y < maxy; y++)
                 {
-                    return new Move();
+                    foreach (var direction in Directions)
+                    {
+                        var location = Move(x, y, direction);
+
+                        if (Valid(location, maxx, maxy))
+                        {
+
+                        }
+                    }
                 }
             }
 
             return null;
         }
+
+        private static bool Valid((int x, int y) coordinates, int maxx, int maxy) =>
+            coordinates.x >= 0 &&
+            coordinates.y >= 0 &&
+            coordinates.x < maxx &&
+            coordinates.y < maxy;
+
+        private static (int x, int y) Move(int x, int y, MoveDirection direction) =>
+            direction switch
+            {
+                MoveDirection.Up    => (x, y + 1),
+                MoveDirection.Down  => (x, y - 1),
+                MoveDirection.Left  => (x - 1, y),
+                MoveDirection.Right => (x + 1, y),
+                _ => throw new ArgumentOutOfRangeException(nameof(direction), direction, null)
+            };
     }
 }
